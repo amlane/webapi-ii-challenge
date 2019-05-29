5fd48a551df369887c1af9e84a5351ae41224182
amlane <twelvexstring@gmail.com>
2019-05-29 14:16:33 -0500

    sending up what I have for comments end points get and post so far, get is working except for 404 error and post isn't working... yet but I think I'm getting closer on it

--- data/postRouter.js
+++ data/postRouter.js
@@ -10,7 +10,7 @@ router.post('/', (req, res) => {
     Posts.insert({ title, contents }).then( newPost => {
         res.status(201).json(newPost);
     }).catch(error => {
-        res.status(500).json({ message: 'There was an error while saving the post to the database.' })
+        res.status(500).json({ error: 'There was an error while saving the post to the database.' })
     })
 })
 
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
     Posts.find().then( post => {
         res.status(200).json(post)
     }).catch(error => {
-        res.status(500).json({ message: 'The posts information could not be retrieved.' })
+        res.status(500).json({ error: 'The posts information could not be retrieved.' })
     })
 })
 
@@ -31,7 +31,7 @@ router.get('/:id', (req, res) => {
             return res.status(200).json(post)
         }
     }).catch(error => {
-        res.status(500).json({ message: 'The posts information could not be retrieved.' })
+        res.status(500).json({ error: 'The posts information could not be retrieved.' })
     })
 })
 
@@ -44,7 +44,7 @@ router.delete('/:id', (req, res) => {
             res.status(204).end();
         }
     }).catch(error => {
-        res.status(500).json({ message: 'The post could not be removed.' })
+        res.status(500).json({ error: 'The post could not be removed.' })
     })
 })
 
@@ -64,10 +64,32 @@ router.put('/:id', (req, res) => {
             res.status(200).json(updated)
         }
     }).catch(error => {
-        res.status(500).json({ message: 'The post information could not be modified.' })
+        res.status(500).json({ error: 'The post information could not be modified.' })
     })
 })
 
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+    
+    Posts.findPostComments(id).then(comments => {
+        if(!comments){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(200).json(comments)
+        }
+    }).catch(error => {
+        res.status(500).json({ error: 'The comments information could not be retrieved.' })
+    })
+})
 
+router.post('/:id/comments', (req, res) => {
+    const { text } = req.body;
+
+    Posts.insertComment({ text }).then( commentText => {
+        res.status(200).json(commentText);
+    } ).catch(error => {
+        res.status(500).json({ error: 'The comments information could not be retrieved.' })
+    })
+})
 
 module.exports = router;
