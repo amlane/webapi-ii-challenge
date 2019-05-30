4790f5d62a0475dd79c6f2bef0e7c03e973526bf
amlane <twelvexstring@gmail.com>
2019-05-30 14:13:33 -0500

    added helmet for security, added logger cuz it's cool custom middleware and also deleted that yarn.lock file b/c I got a package-lock and npm is new and shiny

--- api/server.js
+++ api/server.js
@@ -1,15 +1,24 @@
-const express = require('express'); //import express
+const express = require('express');  //import express
+const helmet = require('helmet');    //import helmet to add security to headers
 
 const postRouter = require('../data/postRouter.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(logger);
 
 server.get('/', (req, res) => {
     res.send('Yo dawg')
 })
 
 server.use('/api/posts', postRouter);
+
+function logger(req, res, next){
+    const time = new Date().toISOString();
+    console.log(`A ${req.method} was made to ${req.url} at ${time}`)
+    next();
+}
 
 module.exports = server;
